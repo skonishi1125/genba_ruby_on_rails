8765d946955e29d79f4232a1aa2673527500aeea
skonishi1125 <skonishi1125@gmail.com>
2023-08-01 22:37:07 +0900

    4-4 コールバック

--- 3_chapter/taskleaf/app/models/task.rb
+++ 3_chapter/taskleaf/app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+    before_validation :set_nameless_name # validationの前にこの処理を呼ぶ
     validates :name, presence: true
     validates :name, length: { maximum: 30 }
     validate :validate_name_not_including_comma
@@ -8,4 +9,10 @@ class Task < ApplicationRecord
     def validate_name_not_including_comma
       errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
     end
+
+    # nameが無記入なら「名前なし」という値を入れる
+    def set_nameless_name
+      self.name = '名前なし' if name.blank?
+    end
+
 end
