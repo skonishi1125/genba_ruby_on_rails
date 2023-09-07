635f72f15bdf88a7fe30cc04870045ad9004965c
skonishi1125 <skonishi1125@gmail.com>
2023-09-07 23:59:46 +0900

    ログイン実装まで

--- 3_chapter/taskleaf/app/controllers/application_controller.rb
+++ 3_chapter/taskleaf/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  before_action :login_required
 
   private
 
@@ -7,4 +8,9 @@ class ApplicationController < ActionController::Base
     # || = とスペースを空けてはいけない
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
+
+  def login_required
+    redirect_to login_url unless current_user
+  end
+
 end
