982445c9b7f24f54d5c7e598a2fd7d92c3428513
skonishi1125 <skonishi1125@gmail.com>
2023-09-28 21:51:22 +0900

    rspec user_b

--- 3_chapter/taskleaf/spec/system/tasks_spec.rb
+++ 3_chapter/taskleaf/spec/system/tasks_spec.rb
@@ -26,5 +26,24 @@ describe 'タスク管理機能', type: :system do
         expect(page).to have_content '最初のタスク'
       end
     end
+
+    context 'ユーザBがログインしているとき' do
+      before do
+        # ユーザーBを作成。
+        user_b = FactoryBot.create(:user, name:'ユーザーB', email: 'b@example.com')
+        # ユーザーBでログイン処理を行う。
+        visit login_path
+        fill_in 'メールアドレス', with: 'b@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
+      end
+
+      it 'ユーザーAが作成したタスクは表示されない' do
+        expect(page).to have_no_content '最初のタスク'
+        # expect(page).not_to have_content '最初のタスク' でも良い
+      end
+    end
+
+
   end
 end
