24c6422e46bb125f63174fdc1029f2f6771ee63e
skonishi1125 <skonishi1125@gmail.com>
2023-09-29 19:58:05 +0900

    no message

--- 3_chapter/taskleaf/spec/system/tasks_spec.rb
+++ 3_chapter/taskleaf/spec/system/tasks_spec.rb
@@ -2,48 +2,33 @@ require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
+    let(:user_a) { FactoryBot.create(:user, name:'ユーザーA', email: 'a@example.com') }
+    let(:user_b) { FactoryBot.create(:user, name:'ユーザーB', email: 'b@example.com') }
+
     before do
-      # ユーザーAを作成しておく
-      user_a = FactoryBot.create(:user, name:'ユーザーA', email: 'a@example.com')
-      # 作成者がユーザーAのタスクを用意しておく
       FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+      # ログイン
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
     end
 
     context 'ユーザーAがログインしているとき' do
-      before do
-        # ユーザーAでログインする(ログインの処理を書いておく)
-          # ログイン画面への遷移
-          visit login_path
-          # メールアドレス、パスワードの入力
-          fill_in 'メールアドレス', with: 'a@example.com'
-          fill_in 'パスワード', with: 'password'
-          # submitボタンの押下
-          click_button 'ログインする'
-      end
-      
+      let(:login_user) { user_a }
+
       it 'ユーザーAが作成したタスクが表示される' do
-        # 作成済みタスクの名称が画面上に表示されていることを確認する
         expect(page).to have_content '最初のタスク'
       end
     end
 
     context 'ユーザBがログインしているとき' do
-      before do
-        # ユーザーBを作成。
-        user_b = FactoryBot.create(:user, name:'ユーザーB', email: 'b@example.com')
-        # ユーザーBでログイン処理を行う。
-        visit login_path
-        fill_in 'メールアドレス', with: 'b@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) { user_b }
 
       it 'ユーザーAが作成したタスクは表示されない' do
         expect(page).to have_no_content '最初のタスク'
-        # expect(page).not_to have_content '最初のタスク' でも良い
       end
     end
-
 
   end
 end
