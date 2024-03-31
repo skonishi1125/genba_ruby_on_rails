83fb7e0817e3eebf47aaf531120ca38aa39fe69f
skonishi1125 <skonishi1125@gmail.com>
2024-03-31 18:30:44 +0900

    色々調整

--- 3_chapter/taskleaf/config/environment.rb
+++ 3_chapter/taskleaf/config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# carrierwave アップローダーとmodel取り付け時の以下のエラーを防ぐ
+# NameError (uninitialized constant User::IconUploader): など
+require 'carrierwave/orm/activerecord'
